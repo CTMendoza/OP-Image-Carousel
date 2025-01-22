e7b36c969b2370f65aecd65750adb37992fc3670
Christopher Mendoza <tiptop1112@gmail.com>
2025-01-22 11:03:35 -0800

    Reset this repo Deleted the unnecessary files from the repo template used for this project but kept the same webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./styles.css";
-import { toggleDropDown } from "./toggleDropDown";
-const dropDownButton = document.querySelector('button');
-const linksContainer = document.querySelector('.link-container');
-
-toggleDropDown();
-
-export {dropDownButton, linksContainer}
--- src/toggleDropDown.js
+++
@@ -1,8 +0,0 @@
-import { dropDownButton, linksContainer } from ".";
-export {toggleDropDown};
-
-function toggleDropDown () {
-dropDownButton.addEventListener('click', ()=> {
-        linksContainer.classList.toggle("open");
-})
-}
